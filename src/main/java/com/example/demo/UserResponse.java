ff7946c08237a22dae4db391d8035c2aabdf707f
chatchanok.wsa <chatchanok.wongsamang@allianz.com>
2020-08-05 11:05:23 +0700

    workshop 07

---
+++ src/main/java/com/example/demo/UserResponse.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+public class UserResponse {
+    private int id;
+    private String name;
+
+    // POJO (Plain Old Java Object)
+    // Getter/Setter methods
+    public UserResponse(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
